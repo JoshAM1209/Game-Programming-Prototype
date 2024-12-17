b57dee57177475f0a1b7f461a0480a5b95956526
Joshua Martin <joshuam120901@gmail.com>
2024-12-17 17:08:01 +0000

    hit detection and pick ups

--- Assets/PlayerManager.cs
+++ Assets/PlayerManager.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
 
 public class PlayerManager : MonoBehaviour
 {
     public int maxHP = 10;
     public int HP;
+    public int score = 0;
+    private int scoreToAdd;
     // Start is called before the first frame update
     void Start()
     {
         HP = maxHP;
     }
-    
-    private void OnTriggerEnter(Collider contact)
+
+    private void OnTriggerEnter2D(Collider2D contact)
     {
         if (contact.gameObject.CompareTag("Damage"))
         {
@@ -23,15 +26,26 @@ public class PlayerManager : MonoBehaviour
                 {
                     GameObject.Destroy(gameObject);
                 }
+                GameObject.Destroy(contact.gameObject);
+            }
+        }
+        
+        if (contact.gameObject.CompareTag("Heal"))
+        {
+            if (HP < maxHP)
+            {
+            HP += 1;
+            GameObject.Destroy(contact.gameObject);
             }
 
-            if (contact.gameObject.CompareTag("Heal"))
-            {
-                if (HP < maxHP)
-                {   
-                    HP += 1;
-                }
-            }
+
+        }
+
+        if (contact.gameObject.CompareTag("Score"))
+        {
+            scoreToAdd = contact.gameObject.GetComponent<ScoreID>().scoreAwarded;
+            score = score + scoreToAdd;
+            GameObject.Destroy(contact.gameObject);
         }
     }
 }
