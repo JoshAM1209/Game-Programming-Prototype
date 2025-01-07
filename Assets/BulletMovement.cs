c8d78dfd4d06f971d4b8a859732de4d7871964e1
Joshua Martin <joshuam120901@gmail.com>
2025-01-07 14:14:49 +0000

    slight change

--- Assets/BulletMovement.cs
+++ Assets/BulletMovement.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class BulletMovement : MonoBehaviour
 {
+    public float bulletLife = 1f;
+    public float rotation = 0f;
+    public float speed = 1f;
+
+    private Vector2 spawnPoint;
+    private float timer = 0f;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        spawnPoint = new Vector2(transform.position)
     }
 
     // Update is called once per frame
